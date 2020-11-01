57743e332c3801d130b0bc9d7b6e19c574e76abe
Hunter Kepley <kepley.l.hunter@gmail.com>
2020-11-01 11:18:25 -0500

    Added poison clouds to sprayers

--- sprayer.py
+++ sprayer.py
@@ -4,15 +4,24 @@ class Sprayer():
     def __init__(self, position):
         self.base_image = pygame.image.load("./Assets/towerbase.png")
         self.top_image = pygame.image.load("./Assets/sprayer.png")
+
         self.base_rect = self.base_image.get_rect()
         self.top_rect = self.top_image.get_rect()
+
         self.base_rect.move_ip(*position)
         self.top_rect.move_ip(*position)
-        self.v = 0
 
-    def update(self, dt):
-        print('',end='')
+        self.rotation = 0
 
+        self.poison_clouds = []
+        self.sprayTimer = 5
+        self.sprayTimerMax = self.sprayTimer
+
+    def update(self, g, dt):
+        self.spray(g)
+
+    def spray(self, g):
+        self.poison_clouds.append(PoisonCloud(self.base_rect.center))
 
     # Static methods
 
@@ -23,17 +32,32 @@ class Sprayer():
     @staticmethod
     def update_sprayers(g, dt):
         for i in g.sprayer_list:
-            i.update(dt)
+            i.update(g, dt)
+            for j in i.poison_clouds:
+                j.update(dt)
 
     @staticmethod
     def render_sprayers(g):
         for i in g.sprayer_list:
             g.screen.blit(i.base_image, i.base_rect)
 
-            i.v+=1
+            i.rotation+=1
 
             center = i.top_rect.center
-            rotated_top_image = pygame.transform.rotate(i.top_image, i.v)
+            rotated_top_image = pygame.transform.rotate(i.top_image, i.rotation)
             new_rect = rotated_top_image.get_rect(center = center)
 
             g.screen.blit(rotated_top_image, new_rect)
+
+            for j in i.poison_clouds:
+                g.screen.blit(j.image, j.rect)
+
+class PoisonCloud():
+    def __init__(self, position):
+        self.image = pygame.image.load("./Assets/selector.png")
+        self.rect = self.image.get_rect()
+        self.rect.move_ip(position)
+        self.move_speed = 1
+
+    def update(self, dt):
+        self.rect.move_ip([self.move_speed, self.move_speed])
